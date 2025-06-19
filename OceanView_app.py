f9e7b73f5912047907bcab4e5e2a5fc25f50d17d
akashspunnayil <akashspunnayil@gmail.com>
2025-06-20 00:25:13 +0530

    updates

--- OceanView_app.py
+++ OceanView_app.py
@@ -215,8 +215,8 @@ if uploaded_file:
                     im.colorbar.set_label(cbar_label, fontsize=12)
 
                 # Add custom axis labels manually
-                ax.text(0.8, -0.1, xlabel, transform=ax.transAxes, ha='center', va='top', fontsize=12)
-                ax.text(-0.1, 0.5, ylabel, transform=ax.transAxes, ha='right', va='center', rotation='vertical', fontsize=12)
+                ax.text(0.5, -0.1, xlabel, transform=ax.transAxes, ha='center', va='top', fontsize=12)
+                ax.text(-0.1, 0.8, ylabel, transform=ax.transAxes, ha='right', va='center', rotation='vertical', fontsize=12)
 
                 # Optional subtitle
                 title = f"{plot_title}"
