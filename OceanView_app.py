f5b1d73bdff94710c2b5b760dc2d9b3f94e00367
akashspunnayil <akashspunnayil@gmail.com>
2025-06-20 01:37:08 +0530

    updates

--- OceanView_app.py
+++ OceanView_app.py
@@ -16,20 +16,12 @@ def load_netcdf_safe(file_obj):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".nc") as tmp_file:
         tmp_file.write(file_obj.read())
         tmp_path = tmp_file.name
-    # try:
-    #     return xr.open_dataset(tmp_path, engine="netcdf4")
-    # except ValueError as e:
-    #     if "unable to decode time units" in str(e) and "calendar 'NOLEAP'" in str(e):
-    #         st.warning("⚠️ Time decoding failed. Retrying with decode_times=False...")
-    #         return xr.open_dataset(tmp_path, decode_times=False, engine="netcdf4")
-
     try:
         return xr.open_dataset(tmp_path, engine="netcdf4")
     except ValueError as e:
-        if "unable to decode time units" in str(e):
-            st.warning("⚠️ Time decoding failed. Retrying with decode_times=True, use_cftime=True...")
-            return xr.open_dataset(tmp_path, decode_times=True, use_cftime=True, engine="netcdf4")
-
+        if "unable to decode time units" in str(e) and "calendar 'NOLEAP'" in str(e):
+            st.warning("⚠️ Time decoding failed. Retrying with decode_times=False...")
+            return xr.open_dataset(tmp_path, decode_times=False, engine="netcdf4")
         else:
             raise
     
