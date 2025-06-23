3a6d9f261bac0df59d660e9da3452b24ed8cf0d1
akashspunnayil <akashspunnayil@gmail.com>
2025-06-23 22:26:08 +0530

    updated app

--- OceanView_app.py
+++ OceanView_app.py
@@ -783,7 +783,7 @@ else:
                                     # -- Add labels with dynamic position
                                     ax_anim.text(0.5, y_offset-0.1, xlabel, transform=ax_anim.transAxes,
                                                  ha='center', va='top', fontsize=label_fontsize)
-                                    ax_anim.text(x_offset-0.2, 0.5, ylabel, transform=ax_anim.transAxes,
+                                    ax_anim.text(x_offset-0.15, 0.5, ylabel, transform=ax_anim.transAxes,
                                                  ha='right', va='center', rotation='vertical', fontsize=label_fontsize)
 
                                 
